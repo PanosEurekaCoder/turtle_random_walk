146125c38a6252ab0223eb8091b5115572485746
Panos <panos.fasoulas@icloud.com>
2024-06-25 00:01:10 +0200

    updated turtle with boundaries random colours added

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
-colors = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue",
-          "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray"]
 orientation = [0, 90, 180, 270]
 melina = Turtle()
 melina.pensize(4)
 melina.speed(0)
 my_screen = Screen()
-boundary = 300
+boundary = 50
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    color = (r, g, b)
+    return color
+
 
 def random_walk(times):
     for _ in range(times):
-        melina.color(random.choice(colors))
+        color_tuple = random_color()
+        melina.color(color_tuple)
         melina.forward(25)
         x, y = melina.position()
         if abs(x) > boundary or abs(y) > boundary:
-            melina.backward(25)  # Move back if out of boundary
+            melina.backward(12)  # Move back if out of boundary
             melina.setheading(random.choice(orientation))  # Change direction
-        choice = random.choice(orientation)
-        melina.setheading(choice)
+        else:
+            choice = random.choice(orientation)
+            melina.setheading(choice)
 
 
 times = int(input("how many times you want Melina to move?: "))
-
 random_walk(times)
 my_screen.exitonclick()
 
